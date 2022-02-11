107ac2cd07bc7a5cc9dde8cb960c8f51c8b9c037
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-11 15:18:58 -0500

    i was using old data

--- 03/puzzle1.js
+++ 03/puzzle1.js
@@ -1,49 +1,41 @@
 const fs = require("fs");
-// let data = fs.readFileSync("../data/raw/03.txt");
+let data = fs.readFileSync("../data/raw/03.txt");
 
-data = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+// data =
+// 	"00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
 
-data = data.toString().split("\n")
-
-console.log(data)
+data = data.toString().split("\n");
 
 let gamma = "";
 for (i = 0; i < data[0].length; i++) {
-    let zeroes = 0;
-    let ones = 0;
-    for (v = 0; v < data.length; v++) {
-        let character = data[v][i]
-        if (character == "0") {
-            zeroes++;
-        }
-        else if (character == "1") {
-            ones++;
-        }
-    }
-    if (zeroes > ones) {
-        gamma += "0";
-    }
-    else {
-        gamma += "1";
-    }
+	let zeroes = 0;
+	let ones = 0;
+	for (v = 0; v < data.length; v++) {
+		let character = data[v][i];
+		if (character == "0") {
+			zeroes++;
+		} else if (character == "1") {
+			ones++;
+		}
+	}
+	if (zeroes > ones) {
+		gamma += "0";
+	} else {
+		gamma += "1";
+	}
 }
+
+console.log(`Gamma Rate: ${gamma} (${parseInt(gamma, 2)})`);
+
 let epsilon = gamma
-    .split('')
-    .map(x => x ^= 1)
-    .join('')
+	.split("")
+	.map((x) => (x ^= 1))
+	.join("");
 
-console.log(epsilon)
+console.log(`Gamma Rate: ${epsilon} (${parseInt(epsilon, 2)})`);
 
-
-console.log(parseInt(gamma, 2))
-console.log(parseInt(epsilon, 2))
-
-// 1000111001011
-// 2^0 + 2^1 + 2^3 + 2^6 + 2^7 + 2^8 + 2^12
-// Gamma rate = 4555
-
-// 0111000110100
-// 2^2 + 2^4 + 2^5 + 2^9 + 2^10 + 2^11
-// Epsilon rate = 3636
-
-// 4555 * 3636 = 16561980
+console.log(
+	`${parseInt(gamma, 2)} * ${parseInt(epsilon, 2)} = ${
+		parseInt(gamma, 2) * parseInt(epsilon, 2)
+	}`
+);
--- 03/puzzle2.js
+++ 03/puzzle2.js
@@ -1,1 +1,28 @@
-let data = require("../data.json")[3];
+const fs = require("fs");
+let data = fs.readFileSync("../data/raw/03.txt");
+
+data =
+	"00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
+
+data = data.toString().split("\n");
+
+for (i = 0; i < data[0].length; i++) {
+	let zeroes = 0;
+	let ones = 0;
+	let zeroIndexes = [];
+	let oneIndexes = [];
+	for (v = 0; v < data.length; v++) {
+		let character = data[v][i];
+		if (character == "0") {
+			zeroes++;
+			zeroIndexes.push(v);
+		} else if (character == "1") {
+			ones++;
+			oneIndexes.push(v);
+		}
+	}
+	console.log(`Zeroes: ${zeroes}`);
+	console.log(`Ones: ${ones}`);
+	console.log(`Zero Indexes: ${zeroIndexes}`);
+	console.log(`One Indexes: ${oneIndexes}`);
+}
