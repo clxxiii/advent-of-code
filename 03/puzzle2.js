46df77619ebc7d47c88f6eadf7856f32c5e4ec53
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-11 15:51:50 -0500

    WHY IS FOR LOOP NOT LOOPIGN

--- 03/puzzle2.js
+++ 03/puzzle2.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
-let data = fs.readFileSync("../data/raw/03.txt");
+// let data = fs.readFileSync("../data/raw/03.txt");
 
 data =
 	"00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
 
 data = data.toString().split("\n");
 
-for (i = 0; i < data[0].length; i++) {
+console.log(dataChecker(data, true));
+
+function dataChecker(data, checkOxy, i) {
+	console.log(data[0].length);
 	let zeroes = 0;
 	let ones = 0;
 	let zeroIndexes = [];
@@ -21,8 +24,23 @@ for (i = 0; i < data[0].length; i++) {
 			oneIndexes.push(v);
 		}
 	}
-	console.log(`Zeroes: ${zeroes}`);
-	console.log(`Ones: ${ones}`);
-	console.log(`Zero Indexes: ${zeroIndexes}`);
-	console.log(`One Indexes: ${oneIndexes}`);
+	console.log(
+		`Zeroes: ${zeroes}\nOnes: ${ones}\nZero Indexes: ${zeroIndexes}\nOne Indexes: ${oneIndexes}`
+	);
+	if (checkOxy) {
+		if (ones >= zeroes) {
+			data = removeIndexes(data, zeroIndexes);
+		} else {
+			data = removeIndexes(data, oneIndexes);
+		}
+	}
+
+	// return data;
+}
+
+function removeIndexes(arr, indexes) {
+	for (i = 0; i < indexes.length; i++) {
+		arr.splice(indexes[i], 1);
+	}
+	return arr;
 }
