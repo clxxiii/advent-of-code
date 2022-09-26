7e97d45ef9d8fe2b6a811254734a3215f33a70ad
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-26 19:13:39 -0400

    problem 7 done

---
+++ 2021/07/puzzle2.ts
@@ -0,0 +1,56 @@
+import { readFileSync } from "fs";
+
+let data: number[] = readFileSync("./2021/data/raw/07.txt")
+	.toString()
+	.split(",")
+	.map((x) => parseInt(x));
+
+let largest = getLargest(data);
+
+let lowestFuel = { position: 0, fuel: NaN };
+for (let i = 0; i <= largest; i++) {
+	let fuel = 0;
+	for (const num of data) {
+		fuel += getDistance(i, num);
+	}
+
+	console.log(`Fuel consumption for ${i}: ${fuel}`);
+
+	if (fuel < lowestFuel.fuel || !lowestFuel.fuel) {
+		lowestFuel = {
+			position: i,
+			fuel,
+		};
+	}
+}
+console.log(
+	`\nLowest Fuel Consumption: ${lowestFuel.fuel} (position ${lowestFuel.position})`
+);
+
+// --- Helper Functions
+
+function getLargest(arr: Array<number>): number {
+	let largest = 0;
+	for (const num of arr) {
+		if (num > largest) {
+			largest = num;
+		}
+	}
+	return largest;
+}
+
+function getDistance(a: number, b: number): number {
+	let difference = Math.abs(a - b);
+	return triangular(difference);
+}
+
+function triangular(n: number): number {
+	if (n == 0) {
+		return 0;
+	}
+	if (n == 1) {
+		return 1;
+	}
+
+	return n + triangular(n - 1);
+}
