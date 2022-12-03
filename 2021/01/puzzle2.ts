03be9335f0b9d7fb28326140de2633e65aa97e91
clxxiii <clxxiii@users.noreply.github.com>
2022-12-03 00:35:59 -0500

    change formatting

--- 2021/01/puzzle2.ts
+++ 2021/01/puzzle2.ts
@@ -1,18 +1,18 @@
 import { readFileSync } from "fs";
 
-let data: Array<number> = readFileSync("./2021/data/raw/01.txt")
-	.toString()
-	.split("\r\n")
-	.map((x) => parseInt(x));
+let data: Array<number> = readFileSync("2021/data/raw/01.txt")
+  .toString()
+  .split("\r\n")
+  .map((x) => parseInt(x));
 
 let incrementCounter = 0;
 for (let i = 3; i < data.length; i++) {
-	let window1 = data[i - 3] + data[i - 2] + data[i - 1];
-	let window2 = data[i - 2] + data[i - 1] + data[i];
+  let window1 = data[i - 3] + data[i - 2] + data[i - 1];
+  let window2 = data[i - 2] + data[i - 1] + data[i];
 
-	if (window2 > window1) {
-		incrementCounter++;
-	}
+  if (window2 > window1) {
+    incrementCounter++;
+  }
 }
 
 console.log(incrementCounter);
