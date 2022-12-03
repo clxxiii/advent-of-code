67559957d30a3c5346857f2a5c0aa9cde06b4e53
clxxiii <clxxiii@users.noreply.github.com>
2022-12-02 23:31:40 -0500

    swap day 1 files

--- 2022/01/puzzle2.ts
+++ 2022/01/puzzle2.ts
@@ -2,22 +2,30 @@ import { readFileSync } from "fs";
 
 let data = readFileSync("2022/data/raw/01.txt").toString().split("\r\n\r\n");
 
-let highest = 0;
+// Sum all elves and put them in an array.
+let sums = [];
 for (const chunk of data) {
-	let elf = chunk
-		.split("\r\n")
-		.filter((x) => x != "")
-		.map((x) => parseInt(x));
+  let elf = chunk
+    .split("\r\n")
+    .filter((x) => x != "")
+    .map((x) => parseInt(x));
 
-	let sum = 0;
-	for (const calorie of elf) {
-		sum += calorie;
-	}
-	console.log(sum);
-	if (sum >= highest) {
-		highest = sum;
-	}
-	console.log(elf);
+  let sum = 0;
+  for (const calorie of elf) {
+    sum += calorie;
+  }
+  sums.push(sum);
 }
 
-console.log(highest);
+console.log(sums);
+
+let highestLimit = 3;
+let highCount = sums.sort((a, b) => b - a);
+highCount.splice(highestLimit, highCount.length - 1);
+console.log(highCount);
+
+let sum = 0;
+for (const num of highCount) {
+  sum += num;
+}
+console.log(sum);
